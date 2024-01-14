0783ad0291ca5507fc2c6d0b4b5c59512ea244e1
LEFGammeltoft-DTU <155906430+LEFGammeltoft-DTU@users.noreply.github.com>
2024-01-14 23:22:06 +0100

    Add files via upload

---
+++ filer_til_projekt/Inc/writename.h
@@ -0,0 +1,16 @@
+/*
+ * controls.h
+ *
+ *  Created on: 12. jan. 2024
+ *      Author: Lauri
+ */
+
+#ifndef WRITENAME_H_
+#define WRITENAME_H_
+#include <stdint.h>
+#include <stdio.h>
+#include "stm32f30x_conf.h" // STM32 config
+#include "30010_io.h"
+char testlives();
+void keyboardex();
+#endif /* WRITENAME_H_ */
